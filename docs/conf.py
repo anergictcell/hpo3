0b676734760e50ff3dd68a8ddaa5d04ea2c74cc5
Jonas Marcello <jonas.marcello@esbme.com>
2025-12-08 21:38:13 +0100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+import pyhpo
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -7,8 +8,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'hpo3'
-copyright = '2023, Jonas Marcello'
+copyright = '2023 - %Y, Jonas Marcello'
 author = 'Jonas Marcello'
+version = pyhpo.__version__
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,6 +31,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "version_selector": True
+}
+
 html_title = 'hpo3 - A Rust implementation of pyhpo'
 html_short_title = 'hpo3'
 
